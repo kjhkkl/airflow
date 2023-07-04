cfe1ad0a6eadd1c149fc512b04a4a0eda0ce3af0
kjhkkl <kjhkkl@naver.com>
2023-07-04 10:02:25 +0900

    수정

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 import random
 
 with DAG(
-    dag_id="dags_bash_operator",
+    dag_id="dags_python_operator",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
@@ -19,3 +19,5 @@ with DAG(
         task_id = 'py_t1',
         python_callable = select_fruit
     )
+
+    py_t1
